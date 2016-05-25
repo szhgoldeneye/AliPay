b096e33229cc1b2c1371a0716073f47c57c29307
He <szhgoldeneye@hotmail.com>
2016-05-25 19:40:02 +0800

    Initial commit

---
+++ src/AliPayController.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+import config.AlipayConfig;
+import util.AlipaySubmit;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/aliapi")
+public class AliPayController {
+
+	@RequestMapping(value = "/deposit", method = RequestMethod.POST)
+	public String deposit(HttpServletRequest request, HttpServletResponse response, Model model) throws Exception {
+		Map<String, String> sParaTemp = new HashMap<String, String>();
+		sParaTemp.put("service", AlipayConfig.service);
+		sParaTemp.put("partner", AlipayConfig.partner);
+		sParaTemp.put("seller_id", AlipayConfig.seller_id);
+		sParaTemp.put("_input_charset", AlipayConfig.input_charset);
+		sParaTemp.put("payment_type", AlipayConfig.payment_type);
+		sParaTemp.put("notify_url", AlipayConfig.notify_url);
+		sParaTemp.put("return_url", AlipayConfig.return_url);
+		sParaTemp.put("anti_phishing_key", AlipayConfig.anti_phishing_key);
+		sParaTemp.put("exter_invoke_ip", AlipayConfig.exter_invoke_ip);
+		sParaTemp.put("out_trade_no", "123");
+		sParaTemp.put("subject", "subject");
+		sParaTemp.put("total_fee", "0.01");
+		sParaTemp.put("body", "body");
+
+		// 建立请求
+		String sHtmlText = AlipaySubmit.buildRequest(sParaTemp, "get", "确认");
+		return sHtmlText;
+	}
+}
