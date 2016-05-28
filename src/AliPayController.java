13a73cc9afa8641f0c4edf2031d7bbd6778f0c5c
He <szhgoldeneye@hotmail.com>
2016-05-28 20:23:04 +0800

    增加回调通知函数

--- src/AliPayController.java
+++ src/AliPayController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/aliapi")
+@RequestMapping(value = "api/ali")
 public class AliPayController {
 
 	@RequestMapping(value = "/pay", method = RequestMethod.POST)
@@ -35,5 +35,35 @@ public class AliPayController {
 		// 建立请求
 		String sHtmlText = AlipaySubmit.buildRequest(sParaTemp, "get", "确认");
 		return sHtmlText;
+	}
+
+	@RequestMapping(value = "/result")
+	public String aliOrderBack(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("ok!!!!!");
+		Map<String, String> params = new HashMap<String, String>();
+		Map<?, ?> requestParams = request.getParameterMap();
+		for (Iterator<?> iter = requestParams.keySet().iterator(); iter.hasNext();) {
+			String name = (String) iter.next();
+			String[] values = (String[]) requestParams.get(name);
+			String valueStr = "";
+			for (int i = 0; i < values.length; i++) {
+				valueStr = (i == values.length - 1) ? valueStr + values[i] : valueStr + values[i] + ",";
+			}
+			params.put(name, valueStr);
+		}
+
+		String out_trade_no = new String(request.getParameter("out_trade_no").getBytes("ISO-8859-1"), "UTF-8");
+		String trade_status = new String(request.getParameter("trade_status").getBytes("ISO-8859-1"), "UTF-8");
+
+		if (AlipayNotify.verify(params)) {
+
+			if (trade_status.equals("TRADE_FINISHED") || trade_status.equals("TRADE_SUCCESS")) {
+
+			}
+
+			return "success";
+		} else {// 验证失败
+			return "fail";
+		}
 	}
 }
