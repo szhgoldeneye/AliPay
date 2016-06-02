23817ec75c16f01e6390af030dc6224fb663daf0
He <szhgoldeneye@hotmail.com>
2016-06-02 15:39:30 +0800

    增加退款异步通知代码

--- src/AliPayController.java
+++ src/AliPayController.java
@@ -97,6 +97,30 @@ public class AliPayController {
 
 	@RequestMapping(value = "/refund/result")
 	public String refundBack(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		return "test";
+		// 获取支付宝POST过来反馈信息
+		Map<String, String> params = new HashMap<String, String>();
+		Map requestParams = request.getParameterMap();
+		for (Iterator iter = requestParams.keySet().iterator(); iter.hasNext();) {
+			String name = (String) iter.next();
+			String[] values = (String[]) requestParams.get(name);
+			String valueStr = "";
+			for (int i = 0; i < values.length; i++) {
+				valueStr = (i == values.length - 1) ? valueStr + values[i] : valueStr + values[i] + ",";
+			}
+			// 乱码解决，这段代码在出现乱码时使用。如果mysign和sign不相等也可以使用这段代码转化
+			// valueStr = new String(valueStr.getBytes("ISO-8859-1"), "gbk");
+			params.put(name, valueStr);
+		}
+
+		String batch_no = new String(request.getParameter("batch_no").getBytes("ISO-8859-1"), "UTF-8");
+		String success_num = new String(request.getParameter("success_num").getBytes("ISO-8859-1"), "UTF-8");
+		String result_details = new String(request.getParameter("result_details").getBytes("ISO-8859-1"), "UTF-8");
+
+		if (AlipayNotify.verify(params)) {// 验证成功
+
+			return "success";
+		} else {// 验证失败
+			return "fail";
+		}
 	}
 }
